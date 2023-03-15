06f251ceefd9b7f827ef3be1fd0d88ca6ca5711d
Jonhnata Farias <jonhnataof@gmail.com>
2023-03-14 21:07:09 -0300

    iniciando projeto

---
+++ src/main/java/com/quadro/atividadesapi/AtividadesApiApplication.java
@@ -0,0 +1,13 @@
+package com.quadro.atividadesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtividadesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtividadesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quadro/atividadesapi/AtividadesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quadro.atividadesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtividadesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
